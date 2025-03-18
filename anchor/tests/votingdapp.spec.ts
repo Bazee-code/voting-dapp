038faaa5c8a2c6f0b07581cf3616ad4779452109
Bazee-code <eugeneobazee2@gmail.com>
2025-03-18 15:20:43 +0300

    Refactor init poll unit test to enable integration testing

--- anchor/tests/votingdapp.spec.ts
+++ anchor/tests/votingdapp.spec.ts
@@ -8,15 +8,24 @@ const IDL = require('../target/idl/votingdapp.json')
 const votingAddress = new PublicKey("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF")
 
 describe('Voting dapp', () => {
-  it('Initialize poll', async () => {
-    const context = await startAnchor("", [{name: "votingdapp", programId: votingAddress}], []);
-    const provider = new BankrunProvider(context);
 
-    const votingProgram = new Program<Votingdapp>(
+  let context;
+  let provider;
+  let votingProgram: anchor.Program<Votingdapp>;
+
+  beforeAll(async () => {
+     context = await startAnchor("", [{name: "votingdapp", programId: votingAddress}], []);
+     provider = new BankrunProvider(context);
+
+     votingProgram = new Program<Votingdapp>(
       IDL,
       provider
     )
 
+  })
+
+  it('Initialize poll', async () => {
+    
     await votingProgram.methods.initializePoll(
       new anchor.BN(1),
       "What is your favorite car brand ?",
@@ -31,14 +40,20 @@ describe('Voting dapp', () => {
     
     // add confirmation of poll address first before fetching
     // await votingProgram.provider.connection.getAccountInfo(pollAddress);
-
+ 
+    // await votingProgram.account.poll
     const poll = await votingProgram.account.poll.fetch(pollAddress);
 
-    console.log('poll',poll);
+    console.log('pollAddy',pollAddress);
+    // console.log('poll', votingProgram.account.poll);
 
     expect(poll.pollId.toNumber()).toEqual(1);
     expect(poll.pollDescription).toEqual("What is your favorite car brand ?");
     expect(poll.pollStartTime.toNumber()).toBeLessThan(poll.pollEndTime.toNumber())
+
+  });
+
+  it('Initialize candidate', async () => {
 
   })
 })
